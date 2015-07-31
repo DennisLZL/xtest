f1dec6e74a26864cf671e112029f8f9e54fffc84
= DL <= DL@mail>
2015-07-31 02:10:44 -0700

    data updating in progress

--- get_stock.py
+++ get_stock.py
@@ -3,17 +3,30 @@ import datetime as dt
 import pandas as pd
 from pyspark import SparkContext
 from os.path import join
+from pywebhdfs.webhdfs import PyWebHdfsClient
 
 def formatLine(line):
     line = line.values()
     line[0], line[5] = line[5], line[0]
     return ', '.join(line)
 
-host = 'hdfs://localhost:9000'
-ticker_path = host + '/user/hadoop/tickers.txt'
-start = '2014-01-01'
-end = dt.date.today().strftime('%Y-%m-%d')
+if __name__ == '__main__':
 
-sc = SparkContext(appName='stock_data')
-stockData = sc.textFile(ticker_path).flatMap(lambda x: Share(x).get_historical(start, end)).map(formatLine)
-stockData.saveAsTextFile(join(host, 'stock', end + '.csv'))
+    host = 'hdfs://localhost:9000'
+    ticker_path = host + '/user/hadoop/tickers.txt'
+    save_path = host + '/user/hadoop/stock'
+    start = '2014-01-01'
+
+    hdfs = PyWebHdfsClient(host='localhost', port='50070', user_name='hadoop')
+    folder = hdfs.list_dir('user/hadoop/stock')['FileStatuses']['FileStatus']
+    files = sorted([dt.datetime.strptime(f['pathSuffix'].split('.')[0], '%Y-%m-%d').date() for f in folder])
+    
+    lastdate = files[-1]
+
+
+
+    #end = dt.date.today().strftime('%Y-%m-%d')
+
+    #sc = SparkContext(appName='stock_data')
+    #stockData = sc.textFile(ticker_path).flatMap(lambda x: Share(x).get_historical(start, end)).map(formatLine)
+    #stockData.saveAsTextFile(join(save_path, end + '.csv'))
